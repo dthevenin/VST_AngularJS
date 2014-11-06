ac19cb04b54bd1d390bb8a7183f9e79d0e1336d9
David Thevenin <david.thevenin@gmail.com>
2014-11-06 14:21:39 +0900

    Add new example

---
+++ examples/ToolBar/toolbar_demo.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var ToolBarApp = angular.module('ToolBarApp', [
+  'ngRoute',
+  'vinisketch'
+]);
+
+ToolBarApp.controller ('AppController', function ($scope) {
+  $scope.panels = [{
+      name: "Buttons",
+      href: "#/",
+    }, {
+      name: "Sliders",
+      href: "#/slidersPanel",
+    }, {
+      name: "Switches",
+      href: "#/switchPanel",
+    }
+  ]
+});
+
+ToolBarApp.config (['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider
+      .when('/slidersPanel', {
+        templateUrl: 'partials/slidersPanel.html'
+      })
+      .when('/switchPanel', {
+        templateUrl: 'partials/switchPanel.html'
+      })
+      .otherwise ({
+        templateUrl: 'partials/buttonsPanels.html'
+      });
+}])
